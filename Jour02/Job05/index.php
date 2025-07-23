524f6a7e2a66737e79aa1f6344c7aa244b503bef
Daria Plishanova <daria.plishanova@laplateforme.io>
2025-07-23 13:08:24 +0200

    Jour 2 finished

---
+++ Jour02/Job05/index.php
@@ -0,0 +1,13 @@
+<?php
+
+function isPrime($nb){//variable for the prim number
+    for($i=2; $i<$nb; $i++) {
+        if($nb % $i===0) return false ;
+    } 
+    return true;
+}
+
+for($i=2; $i<= 1000; $i++) {
+    if(isPrime($i)) echo "$i <br/>";
+}
+
